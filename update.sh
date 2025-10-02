6f83ea1e313df4af3bd5f4174143237826520dea
domster704 <domster704@mail.ru>
2025-10-02 22:39:09 +0300

    finish

--- update.sh
+++ update.sh
@@ -5,3 +5,6 @@ git submodule update --init --remote
 
 docker compose down &&
 docker compose up --build -d
+
+cd lct2025-backend
+docker compose -f ./run/docker-compose.yml down && docker compose -f ./run/docker-compose.yml up --build -d
