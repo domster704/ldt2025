8826fedec0844d8d07dcc532230f3e0d754a94df
domster704 <domster704@mail.ru>
2025-10-02 23:22:05 +0300

    fix update.sh

--- update.sh
+++ update.sh
@@ -3,8 +3,9 @@
 git reset --hard HEAD && git pull
 git submodule update --init --remote
 
-sudo docker compose down &&
-sudo docker compose up --build -d
+sudo docker compose -f ./docker-compose.yml down &&
+sudo docker compose -f ./docker-compose.yml up --build -d
 
 cd lct2025-backend
-sudo docker compose -f ./run/docker-compose.yml down && sudo docker compose -f ./run/docker-compose.yml up --build -d
+sudo docker compose -f ./run/docker-compose.yml down &&
+ sudo docker compose -f ./run/docker-compose.yml up --build -d
